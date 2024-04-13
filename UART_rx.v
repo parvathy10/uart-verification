ab72fdfafde0c55c2a6538b44218db5d0f926137
Parvathy Harikumar <parvathyharikumar01@gmail.com>
2024-04-13 23:41:37 +0530

    modified commit

---
+++ UART_rx.v
@@ -0,0 +1,75 @@
+module uart_slave(
+    input clk,    //clock
+    input u_rx,    //input form another uart
+    output [7:0]data,  //output to main bus or other module
+    input en_rx, //enable signal to Rx UART
+    output reg u_rx_done //Rx is done
+);
+
+reg [2:0] state_rx;
+reg [3:0] count;
+reg [7:0] dout;
+
+
+parameter IDLE = 3'b000;
+parameter START = 3'b001;
+parameter DATA = 3'b010;
+parameter PARITY = 3'b011;
+parameter DONE = 3'b100;
+
+always @(negedge clk ) begin
+    
+    case (state_rx)
+    IDLE: begin
+        if (en_rx) begin
+            count <= 0;
+            u_rx_done <= 0;
+        end
+        else begin
+            state_rx <= IDLE;
+        end
+
+        if (u_rx == 0) begin
+            state_rx <= DATA;
+        end
+        else begin
+                state_rx <= IDLE;
+        end
+
+    end
+
+    DATA: begin
+        dout[count] <= u_rx;
+        if (count == 3'b111) begin
+            state_rx <= PARITY;
+        end
+        else begin
+            state_rx <= DATA;
+            count <= count + 1;
+        end
+    end
+
+    PARITY: begin
+        if (u_rx == ^dout) begin
+            state_rx <= DONE;
+          	 u_rx_done <= 1;
+        end
+        else begin
+            state_rx <= IDLE;
+        end
+    end
+
+    DONE: begin
+        state_rx <= IDLE;
+    end
+
+    default: begin
+        state_rx <= IDLE;
+    end
+
+    endcase
+end
+
+assign data = ((state_rx == DONE)&(en_rx)) ? dout : 8'b00000000;
+
+endmodule
---
+++ UART_tx.v
@@ -0,0 +1,73 @@
+
+
+module uart_master(
+    input clk, //clock
+    input [7:0]data, //input from main bus or other module
+    input en_tx, //enable signal to Tx UART 
+    // input en_rx,
+    output reg u_tx, //output to another uart 
+    output reg u_tx_done //Tx is done
+    // u_rx_done,
+    // input u_rx
+);
+
+reg [2:0] state_tx;
+// reg [2:0] state_rx;
+reg [2:0] count;
+reg [7:0] din;
+
+parameter IDLE = 3'b000;
+parameter START = 3'b001;
+parameter DATA = 3'b010;
+parameter PARITY = 3'b011;
+parameter DONE = 3'b100;
+
+always @(negedge clk ) begin
+    case (state_tx)
+
+    START: begin
+        if (en_tx) begin
+            state_tx <= DATA;
+            din <= data;
+            u_tx <= 0;
+            u_tx_done <= 0;
+            count <= 0;
+        end
+        else begin
+            u_tx <= 1'bz;
+            // state_tx <= START;
+        end
+        // u_tx <= 0;
+        // state_tx <= DATA;
+        // count <= 0;
+        
+    end
+
+    DATA: begin
+        count <= count + 1;
+        if (count == 3'b111) begin
+            u_tx <= din[count];
+            state_tx <= PARITY;
+        end
+        else u_tx <= din[count];
+    end
+
+    PARITY: begin
+        u_tx <= ^din;  // even parity
+        state_tx <= DONE;
+    end
+
+    DONE: begin
+        u_tx_done <= 1;
+        u_tx <= 0;
+        state_tx <= START;
+    end
+    
+    default: begin
+        state_tx <= START;
+    end
+    endcase
+end
+
+endmodule
+  
